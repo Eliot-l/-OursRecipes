ab9dfed04b2251d794ceee841a464e4326be8f36
eliot delmas <delmaseliot@gmail.com>
2025-01-15 12:02:20 +0100

    Setup Models

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,57 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_01_15_094901) do
+ActiveRecord::Schema[7.1].define(version: 2025_01_15_105944) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.text "content"
+    t.bigint "user_id", null: false
+    t.bigint "recipe_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["recipe_id"], name: "index_comments_on_recipe_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
+  create_table "ingredients", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "recipe_ingredients", force: :cascade do |t|
+    t.integer "quantity"
+    t.string "unit"
+    t.bigint "ingredient_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["ingredient_id"], name: "index_recipe_ingredients_on_ingredient_id"
+  end
+
+  create_table "recipes", force: :cascade do |t|
+    t.text "description"
+    t.string "name"
+    t.integer "duration"
+    t.integer "rating"
+    t.string "difficulty"
+    t.integer "price"
+    t.bigint "recipe_ingredient_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["recipe_ingredient_id"], name: "index_recipes_on_recipe_ingredient_id"
+    t.index ["user_id"], name: "index_recipes_on_user_id"
+  end
+
+  create_table "steps", force: :cascade do |t|
+    t.string "description"
+    t.bigint "recipe_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["recipe_id"], name: "index_steps_on_recipe_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -26,4 +74,10 @@ ActiveRecord::Schema[7.1].define(version: 2025_01_15_094901) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "comments", "recipes"
+  add_foreign_key "comments", "users"
+  add_foreign_key "recipe_ingredients", "ingredients"
+  add_foreign_key "recipes", "recipe_ingredients"
+  add_foreign_key "recipes", "users"
+  add_foreign_key "steps", "recipes"
 end
